6f44162a8ec081fc6f8ab9e391482ee260a6e980
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-09 18:14:01 +0300

    Update operations.js

--- src/redux/userBalance/operations.js
+++ src/redux/userBalance/operations.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import * as api from 'services/kapusta-api';
-
+import * as api from '../../components/BalanceForm/balanceApi';
+// import * as api from 'services/kapusta-api'; потом поменять если что на такое
 const fetchUser = createAsyncThunk('user', async () => {
   try {
     const { data } = await api.getUser();
