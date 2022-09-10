2a2ae34c4def261999bff6ba0cc4fc122b671f9b
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-10 14:12:11 +0300

    Разметка поле баланс и окно введите баланс

---
+++ src/components/BalanceForm/BalanceForm.jsx
@@ -0,0 +1,47 @@
+import s from './BalanceForm.module.css';
+
+import Toast from 'components/BalanceFormToast';
+
+const BalanceForm = () => {
+  return (
+    <div className={s.Container}>
+      <p className={s.Title}>Balance:</p>
+      <form
+        className={s.Box}
+        // onSubmit={handleSubmit}
+      >
+        {/* // {canChange ? ( */}
+        <input
+          className={s.Value}
+          // value={balanceInput}
+          placeholder={'0 UAH'}
+          // onChange={handleChange}
+          // onBlur={handleBlur}
+          // onFocus={handleFocus}
+        />
+        {/* // ) : ( */}
+        {/* <span className={s.Value}>
+          {balanceInput}
+        </span> */}
+        {/* // )} */}
+        <button
+          type={'submit'}
+          className={s.ButtonDisabled}
+          // {canChange ? s.ButtonEnabled : s.ButtonDisabled}
+        >
+          CONFIRM
+        </button>
+        <Toast onClose={() => {}} />
+        {/* ------------------------ */}
+        {/* {isToastShown && (
+        <Toast
+        onClose={() => setIsToastShown(false)}
+        />
+        )} */}
+        {/* ------------------------- */}
+      </form>
+    </div>
+  );
+};
+
+export default BalanceForm;
