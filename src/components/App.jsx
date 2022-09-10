47b014799482ab7fd0de2045de0bd85a7ccf375b
Oleg <hellreebok@gmail.com>
2022-09-10 17:52:03 +0300

    Update Header and LoginForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import { SharedLayout } from 'components';
-import { Home, Expenses, Income, Reports, PageNotFound } from 'pages';
+import { SharedLayout, LogInForm } from 'components';
+import { Expenses, Income, Reports, PageNotFound } from 'pages';
 
 export const App = () => {
     return (
         <Routes>
             <Route path="/" element={<SharedLayout />}>
-                <Route index element={<Home />} />
+                <Route index element={<LogInForm />} />
                 <Route path="expenses" element={<Expenses />} />
                 <Route path="income" element={<Income />} />
                 <Route path="reports" element={<Reports />} />
