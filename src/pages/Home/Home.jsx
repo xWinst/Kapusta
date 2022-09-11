0ab41007914b04216f8a77ee6978744af5035ac0
AleksZhov <alekszhov@gmail.com>
2022-09-11 22:15:38 +0300

    Added page home and styled

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
-// import s from '../index.module.css';
-import LogInForm from 'components/LogInForm/LogInForm';
+import s from './Home.module.css';
+import { LogInForm } from 'components';
+import kapustaText from '../../images/union.svg';
+
 const Home = () => {
     return (
-        <div>
-            Home
-            <LogInForm />
+        <div className={s.loginSectWrapper}>
+            <div className={s.mainTextWrapper}>
+                <img src={kapustaText} alt="Kapusta" />
+                <h2 className={s.loginSectText}>Smart Finance</h2>
+            </div>
+            <div className={s.loginFormWrapper}>
+                <LogInForm />
+            </div>
         </div>
     );
 };
