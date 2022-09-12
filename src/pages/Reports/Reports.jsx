a3a60bd91da52d126651c35d01bd559d2b6bf0c1
kate-zahorui <kate.zahorui@gmail.com>
2022-09-12 07:55:30 +0300

    Separates Period and ButtonBack components

--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,30 +1,17 @@
-import { Link } from 'react-router-dom';
-
-import { FlipButtons } from '../../components/FlipButtons/FlipButtons';
+import { ButtonBack } from '../../components/ButtonBack/ButtonBack';
+import { ReportsPeriod } from '../../components/ReportsPeriod/ReportsPeriod';
 import { ReportsTotal } from '../../components/ReportsTotal/ReportsTotal';
 import { ReportsCategories } from '../../components/ReportsCategories/ReportsCategories';
 
 import s from './Reports.module.css';
-import svg from '../../images/sprite.svg';
 
 const Reports = () => {
     return (
         <div className={s.container}>
             <div className={s.reports__nav}>
-                <Link to="/expenses" className={s.back}>
-                    <svg className={s.back__icon} width="24" height="24">
-                        <use href={`${svg}#icon-ArrowToGoBack`}></use>
-                    </svg>
-                    <span className={s.back__text}>Main page</span>
-                </Link>
-
+                <ButtonBack />
                 <p>Balance</p>
-                <div className={s.period}>
-                    <p className={s.period__text}>Current period:</p>
-                    <FlipButtons>
-                        <h2 className={s.period__title}>November 2019</h2>
-                    </FlipButtons>
-                </div>
+                <ReportsPeriod />
             </div>
             <ReportsTotal />
             <ReportsCategories />
