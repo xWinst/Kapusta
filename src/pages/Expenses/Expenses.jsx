b3f5745fde9a95da10bf0e6a8f0c94fbed419b7f
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-09-12 12:33:06 +0300

    Add report icon on Expenses.jsx

--- src/pages/Expenses/Expenses.jsx
+++ src/pages/Expenses/Expenses.jsx
@@ -2,12 +2,17 @@ import s from '../index.module.css';
 import { Link } from 'react-router-dom';
 import { ExpensesModule } from 'modules';
 import { BalanceForm } from 'components';
+import { ReactComponent as ReportPic } from 'images/barChart.svg';
+import style from './Expenses.module.css';
 
 const Expenses = () => {
     return (
         <>
             <div className={s.container}>
-                <Link to="/reports">Reports</Link>
+                <Link to="/reports" className={style.goToReport}>
+                    Reports
+                    <ReportPic className={style.reportPic} alt={'reportPic'} />
+                </Link>
                 <BalanceForm />
             </div>
             <ExpensesModule />
