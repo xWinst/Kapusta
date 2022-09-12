b9f5c21142641d78186612a0fe89b05bf07f2b47
Oleg <hellreebok@gmail.com>
2022-09-12 15:25:08 +0300

    del warning

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -2,7 +2,7 @@ const getState = state => state;
 
 const getUserEmail = state => state.auth.userData.email;
 
-const getBalance = state => state.auth.userData.balance;
+// const getBalance = state => state.auth.userData.balance;
 
 const getIsLoggedIn = state => state.auth.isLoggedIn;
 
