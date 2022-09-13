b24599e1613c6e7344651a27523539c48c0fab31
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2022-09-13 13:45:36 +0300

    Fixed styles of mobili input

--- src/redux/transaction/transactionOperations.js
+++ src/redux/transaction/transactionOperations.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
