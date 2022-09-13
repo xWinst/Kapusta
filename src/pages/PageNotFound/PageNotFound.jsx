6b7cc5404059f13ba9b6f9c81bb202fefb8f545d
kate-zahorui <kate.zahorui@gmail.com>
2022-09-13 18:06:47 +0300

    Adds custom PageNotFound

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,33 @@
-// import s from '../index.module.css';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../../components/Button/Button';
+import s from './PageNotFound.module.css';
 
 const PageNotFound = () => {
-  return <div>404 Page Not Found</div>;
+    const navigate = useNavigate();
+
+    const handleBtnClick = () => {
+        navigate('/', { replace: true });
+    };
+
+    return (
+        <div className={s.container}>
+            <div className={s.background}>
+                <h2 className={s.title}>404 Page Not Found</h2>
+                <div className={s.img}></div>
+                <p className={s.text}>
+                    Oops! The page you're looking for doesn't exist.
+                </p>
+
+                <Button
+                    type={'button'}
+                    buttonName={'gohome'}
+                    title={'Go home'}
+                    onClick={handleBtnClick}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default PageNotFound;
