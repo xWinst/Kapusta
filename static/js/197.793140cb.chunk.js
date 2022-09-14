e9a58ba22a8df4b6cefd859e22fdd5028cadaa69
xWinst <36066519+xWinst@users.noreply.github.com>
2022-09-14 15:06:22 +0000

    Deploying to gh-pages from @ xWinst/Kapusta@2712c725bd32ec635ef1198f66fceeb90d33d9fb 🚀

---
+++ static/js/197.793140cb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkapusta=self.webpackChunkkapusta||[]).push([[197],{1197:function(e,s,t){t.r(s);var n=t(7806),i=t(9368),u=t(9434),a=t(6871),p=t(184);s.default=function(){var e=(0,u.v9)(i.u.getIsLoggedIn),s=(0,a.TH)().pathname;return e?(0,p.jsxs)(p.Fragment,{children:["/expenses-input-mobile"===s&&(0,p.jsx)(n.iP,{title:"Expenses"}),"/incomes-input-mobile"===s&&(0,p.jsx)(n.iP,{title:"Incomes"})]}):(0,p.jsx)(a.Fg,{to:"/"})}}}]);
+//# sourceMappingURL=197.793140cb.chunk.js.map
