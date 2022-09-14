d5b54d3bdf9ca0bfc7a4afff31dde99b1f4a6cc1
kate-zahorui <kate.zahorui@gmail.com>
2022-09-14 12:49:18 +0300

    Fixes stats in Summary

--- src/components/Balance/BalanceSummary/BalanceSummary.js
+++ src/components/Balance/BalanceSummary/BalanceSummary.js
@@ -1,19 +1,33 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { getExpense, getIncome } from 'redux/transaction/transactionOperations';
 
 import months from '../../../months.json';
 
 import s from '../BalanceSummary/BalanceSummary.module.css';
 
 export default function BalanceSummary() {
+    const dispatch = useDispatch();
     const { pathname } = useLocation();
 
+    const isLoggedIn = useSelector(state => state.finance.isLoggedIn);
     const userExpensesStats = useSelector(
         state => state.finance.userExpenses.monthsStats
     );
     const userIncomeStats = useSelector(
         state => state.finance.userIncomes.monthsStats
     );
+    const userTransactions = useSelector(
+        state => state.finance.userData.transactions
+    );
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            dispatch(getExpense());
+            dispatch(getIncome());
+        }
+    }, [dispatch, isLoggedIn, userTransactions]);
 
     const getMonths = i => {
         return months[i];
