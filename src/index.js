68218042560943c70c4ac2f7b9c3a7babc89dca9
Oleg <36066519+xWinst@users.noreply.github.com>
2022-09-15 08:25:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { store, persistor } from 'redux/store';
 import { GoogleOAuthProvider } from '@moeindana/google-oauth';
 import './index.css';
 
+console.log('process.env: ', process.env);
 console.log('process.env.REACT_APP_CLIENT_ID: ', process.env.REACT_APP_CLIENT_ID);
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
